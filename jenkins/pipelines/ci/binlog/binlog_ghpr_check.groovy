ee68abdb6ba7cd93fa2fcda8cf2d1b77bc758982
purelind <purelind@gmail.com>
2021-05-14 16:33:42 +0800

    add binlog pipeline

---
+++ jenkins/pipelines/ci/binlog/binlog_ghpr_check.groovy
@@ -0,0 +1,82 @@
+node("${GO_TEST_SLAVE}") {
+    def ws = pwd()
+    deleteDir()
+
+    dir("/home/jenkins/agent/git/tidb-binlog") {
+        stage("Prepare") {
+            println "debug command:\nkubectl -n jenkins-ci exec -ti ${NODE_NAME} bash"
+
+            if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                deleteDir()
+            }
+            if (ghprbTargetBranch == "master") {
+                echo "Target branch: ${ghprbTargetBranch}, load script from pr..."
+                checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: 'pull/${ghprbPullId}/head', url: 'git@github.com:pingcap/tidb-binlog.git']]]
+                // 等到大多数 PR update  master 后可以取消下面的注释
+                // sh"""
+                // git checkout -f ${ghprbActualCommit}
+                // """
+            } else {
+                echo "Target branch: ${ghprbTargetBranch}, load script from master..."
+                checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', url: 'git@github.com:pingcap/tidb-binlog.git']]]
+            }
+        }
+    }
+
+    catchError {
+        node("build_go1130_binlog") {
+            deleteDir()
+            def ws = pwd()
+            container("golang") {
+                dir("/home/jenkins/agent/git/tidb-binlog") {
+                    if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                        deleteDir()
+                    }
+                    try {
+                        checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/pull/*:refs/remotes/origin/pr/*', url: 'git@github.com:pingcap/tidb-binlog.git']]]
+                    } catch (error) {
+                        retry(2) {
+                            echo "checkout failed, retry.."
+                            sleep 60
+                            if (sh(returnStatus: true, script: '[ -d .git ] && [ -f Makefile ] && git rev-parse --git-dir > /dev/null 2>&1') != 0) {
+                                deleteDir()
+                            }
+                            checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [[$class: 'PruneStaleBranch'], [$class: 'CleanBeforeCheckout']], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'github-sre-bot-ssh', refspec: '+refs/pull/*:refs/remotes/origin/pr/*', url: 'git@github.com:pingcap/tidb-binlog.git']]]
+                        }
+                    }
+                }
+
+                dir("go/src/github.com/pingcap/tidb-binlog") {
+                    stage('Build') {
+                        sh """
+                            cp -R /home/jenkins/agent/git/tidb-binlog/. ./
+                            git checkout -f ${ghprbActualCommit}
+                            GOPATH=\$GOPATH:${ws}/go make check
+                        """
+                    }
+                }
+            }
+        }
+
+        currentBuild.result = "SUCCESS"
+    }
+
+    stage('Summary') {
+        def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
+        def slackmsg = "[#${ghprbPullId}: ${ghprbPullTitle}]" + "\n" +
+                "${ghprbPullLink}" + "\n" +
+                "${ghprbPullDescription}" + "\n" +
+                "Build Result: `${currentBuild.result}`" + "\n" +
+                "Elapsed Time: `${duration} mins` " + "\n" +
+                "${env.RUN_DISPLAY_URL}"
+
+        if (currentBuild.result != "SUCCESS") {
+            slackSend channel: '#jenkins-ci', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+        }
+    }
+
+}
+
+
+
+
